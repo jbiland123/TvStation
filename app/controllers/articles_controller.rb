d9706fca44450c3ea11213286f0bcc97a76e966a
jbiland123 <jeremias.biland@gmail.com>
2023-08-17 11:41:12 +0200

    Weather 3.2

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -25,12 +25,37 @@ class ArticlesController < ApplicationController
       formatted_time
     end
 
-    url2 = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/liestal?unitGroup=metric&key=WMGTGTC8KQ68VLT8C62HQJCMW&contentType=json&lang=de'
-    uri2 = URI(url2)
-    res2 = Net::HTTP.get_response(uri2)
-    @data2 = JSON.parse(res2.body)
+    daily_temperatures = {}
 
-    @days = @data2["days"]
+    @data1["list"].each do |interval|
+      date = interval["dt_txt"].split[0]
+      min_temp = interval["main"]["temp_min"]
+      max_temp = interval["main"]["temp_max"]
+
+      if daily_temperatures[date]
+        daily_temperatures[date][:min] = [daily_temperatures[date][:min], min_temp].min
+        daily_temperatures[date][:max] = [daily_temperatures[date][:max], max_temp].max
+      else
+        daily_temperatures[date] = { min: min_temp, max: max_temp }
+      end
+    end
+
+    @daily_temperatures = daily_temperatures
+
+    daily_weather_icons = {}
+
+    @data1["list"].each do |interval|
+      time = interval["dt_txt"].split[1]
+      date = interval["dt_txt"].split[0]
+
+      if time == "12:00:00"
+        weather_icon = interval["weather"][0]["icon"]
+        daily_weather_icons[date] = weather_icon
+      end
+    end
+
+    @daily_weather_icons = daily_weather_icons
+
 
     url3 = 'https://newsapi.org/v2/top-headlines?country=ch&apiKey=5198179471974a11ba335b68f308f387'
     uri3 = URI(url3)
