76ea6934b4a5228002d287e36684c641974a6a75
jbiland123 <jeremias.biland@gmail.com>
2024-01-11 08:12:09 +0100

    update

--- public/assets/updateTime-7e36a981de55ddda091d655b883636391ff9aa1a5c512e0e8afaa23cfad677d8.js
+++
@@ -1,9 +0,0 @@
-function updateTime() {
-    const currentTime = new Date();
-    const options = { weekday: 'long', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' };
-    const formattedTime = currentTime.toLocaleDateString('de', options);
-    document.getElementById('current-time').innerText = formattedTime;
-  }
-  updateTime();
-  setInterval(updateTime, 1000);
-  
